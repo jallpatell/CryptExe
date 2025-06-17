8501f0b2a18a4eb78dae3ae1bc71c5d34b458a81
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-06-18 04:15:00 +0530

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import CryptexLanding from './pages/Home'
 import Portfolio from './pages/Portfolio'
@@ -12,14 +12,14 @@ import Wallet from './pages/Wallet'
 export default function App() {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 overflow-y-auto overscroll-none bg-gray-900'>
-<BrowserRouter>
+<HashRouter>
         <Routes>
           <Route path="/" element={<CryptexLanding />} />
           <Route path="/wallet" element={<Wallet />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
       
 
